316296b252f139c07e39da847209ded78db159a3
steveAzo -Git <stephenazongo77@gmail.com>
2025-03-27 07:59:28 +0000

    Fixed Swagger docs

--- swaggerConfig.js
+++ swaggerConfig.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: config.BASEURL || "http://localhost:3000", 
+        url: config.BASEURL || "https://kada-backend.onrender.com/", 
         description: 'Development server',
       },
     ],
